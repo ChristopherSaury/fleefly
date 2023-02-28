ed9fdd5f655c59a19c511acb69b461df78f85981
ChristopherSaury <saurychristopher@gmail.com>
2023-02-28 10:27:47 +0100

    router

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 require __DIR__ . '/Config.php';
+require __DIR__ . '/functions/Functions.php';
+
+if ($_GET['route']){
+    $route = $_GET['route'];
+}
+
+switch ($route || empty($route)){
+
+}
