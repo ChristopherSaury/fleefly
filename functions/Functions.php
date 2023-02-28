dfbfa71d89b3cd22cecaa69fd94baa15dfdd7212
ChristopherSaury <saurychristopher@gmail.com>
2023-02-28 17:02:54 +0100

    class Functions

--- functions/Functions.php
+++ functions/Functions.php
@@ -1,5 +1,13 @@
 <?php 
 
-class Functions {
-    
+class Functions{
+    public static function view (string $file){
+        return Config::VIEWS . '/' . $file . '.php';
+    }
+    public static function asset (string $file){
+        return Config::BASE_URL . '/../asset/' . $file;
+    }
+    public static function url(string $route){
+        return Config::BASE_URL . '?route=' . $route;
+    }
 }
