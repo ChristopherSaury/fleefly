c66add85d40af98b4ecd4a2e93d1a8eb2fa8dc46
ChristopherSaury <saurychristopher@gmail.com>
2023-03-05 15:58:41 +0100

    destinations page view

---
+++ view/destinations.html.php
@@ -0,0 +1,75 @@
+<?php require Functions::view('components/header.html'); ?>
+
+<section id="destinations">
+    <div class="dscreen">
+        <h2>Destinations</h2>
+    </div>
+    <div class="dcard-section">
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/south-africa.jpg'); ?>" alt="South Africa photo">
+                <h4 class="dcard-title">South Africa</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/peru.jpg'); ?>" alt="Peru photo">
+                <h4 class="dcard-title">Peru</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/vietnam.jpg'); ?>" alt="Vietnam photo">
+                <h4 class="dcard-title">Vietnam</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/china.jpg'); ?>" alt="China photo">
+                <h4 class="dcard-title">China</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/tanzania.jpg'); ?>" alt="Tanzania photo">
+                <h4 class="dcard-title">Tanzania</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/norway.jpg'); ?>" alt="Norway photo">
+                <h4 class="dcard-title">Norway</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/hungary.jpg'); ?>" alt="Hungary photo">
+                <h4 class="dcard-title">Hungary</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/portugal.jpg'); ?>" alt="Portugal photo">
+                <h4 class="dcard-title">Portugal</h4>
+            </a>
+        </div>
+
+        <div class="dcard">
+            <a class="dcard-photo" href="#">
+                <img src="<?= Functions::asset('img/destinations/brasil.jpg'); ?>" alt="Brasil photo">
+                <h4 class="dcard-title">Brasil</h4>
+            </a>
+        </div>
+
+    </div>
+</section>
+
+<?php require Functions::view('components/footer.html'); ?>
